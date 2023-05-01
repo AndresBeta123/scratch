1ea518242f4c0f5ff3f8c4c4b54f67b4dce1e80e
Cesar SC <csolanoc@unal.edu.co>
2023-05-01 18:44:07 -0500

    Creo que este es

--- escenario1.cc
+++ escenario1.cc
@@ -45,6 +45,7 @@ uint32_t numPackets = 1;
 uint32_t numNodes = 6;  // by default, 5x5
 uint32_t sinkNode = 1;
 uint32_t sourceNode = 0;
+double tiempoEnvios = 120.0;
 int capacidadCap2=1;
 int numClustersCap1 = 4; // Escenario 1
 int numClustersCap2 = 2;
@@ -83,7 +84,7 @@ void createOnOff(NodeContainer sourceNodeContainer, uint32_t sourceClusterIndex,
   // cout << to_string(capacidadCap2) + "-" << to_string(numClustersCap1) + "-" << to_string(static_cast<double>(capacidadCap2)/numClustersCap1) << to_string((dataRate*(capacidadCap2/numClustersCap1))) << endl;
   cout <<to_string(expTime->GetValue())+ ","+ to_string(expTime->GetValue()) << endl;
   app.Start(Seconds(30.0));
-  app.Stop(Seconds(60.0)); 
+  app.Stop(Seconds(tiempoEnvios)); 
 }
 
 void createOnOffRandom(NodeContainer sourceNodeContainer, NodeContainer sinkNodeContainer, Ipv4InterfaceContainer sinkInterface, double dataRate, uint32_t packetSize){
@@ -262,13 +263,13 @@ int main (int argc, char *argv[])
 
   for (int i = 0; i < numClustersCap1; i++)
   {
-    for (int j = 1; j < numNodesCap1; j++){
+    for (int j = 0; j < numNodesCap1; j++){
 
       uint32_t sinkCluster = clusterUniform->GetValue();
       uint32_t sinkNode = nodeUniform->GetValue();
 
-      double dataRate = geometric_truncated(j, 0.7, numNodesCap1);
-      createOnOff(c1Cluster[i], i,  j, c1Cluster[sinkCluster], sinkCluster,  sinkNode, d1[i], dataRate, 1024); 
+      double dataRate = geometric_truncated(j, 0.1, numNodesCap1);
+      createOnOff(c1Cluster[i], i,  j, c1Cluster[sinkCluster], sinkCluster,  sinkNode, d1[i], dataRate, 256); 
     }
   }
   
@@ -279,7 +280,7 @@ int main (int argc, char *argv[])
 
   AsciiTraceHelper ascii;
   wifiPhy[0].EnableAsciiAll(ascii.CreateFileStream("ando.tr"));
-  Simulator::Stop (Seconds (60.0));
+  Simulator::Stop (Seconds (tiempoEnvios));
   Simulator::Run ();
   Simulator::Destroy ();
 
